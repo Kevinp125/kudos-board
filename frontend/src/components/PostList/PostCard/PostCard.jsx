fadab1582f15115e8c87f421ab1368f1c52eca54
Kevin Pereda <kevinpereda125@meta.com>
2025-06-16 22:13:18 -0700

    Styled postcard so it is a little different than its doppleganger BoardCard.

--- frontend/src/components/PostList/PostCard/PostCard.jsx
+++ frontend/src/components/PostList/PostCard/PostCard.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import "./post-card.css";
 
-export default function PostCard() {
-  return <p>hewo</p>;
+export default function PostCard({ post }) {
+  return (
+    <article className="post-card">
+      <h2>{post.title}</h2>
+      <p>{post.message}</p>
+      <img
+        className="post-image"
+        src="/placeholderboard.png"
+        alt={`placeholder image`}
+      />
+
+      <div className="post-btn-container">
+        <button>Upvote</button>
+        <button className="deletebtn">Delete</button>
+      </div>
+    </article>
+  );
 }
