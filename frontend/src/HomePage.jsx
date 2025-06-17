dce39de06c0e682c8a360be7898698f1d0aee1bf
Kevin Pereda <kevinpereda125@meta.com>
2025-06-16 20:49:52 -0700

    got my code formatter to work all of these changes are just spacing and indents being updatedgit add .

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -1,22 +1,17 @@
-import { useState } from 'react'
-import './HomePage.css'
-import { Link } from 'react-router-dom'
+import { useState } from "react";
+import "./HomePage.css";
+import { Link } from "react-router-dom";
 
-import Header from './components/Header/Header'
-import BoardList from './components/BoardList/BoardList'
-import Footer from './components/Footer/Footer'
+import Header from "./components/Header/Header";
+import BoardList from "./components/BoardList/BoardList";
+import Footer from "./components/Footer/Footer";
 
 export default function HomePage() {
-
   return (
-
-    <div className = "homepage-container">
-      <Header/>
-      <BoardList/>
-      <Footer/>
-
-
+    <div className="homepage-container">
+      <Header />
+      <BoardList />
+      <Footer />
     </div>
-
-  )
+  );
 }
--- frontend/src/components/BoardList/BoardCard/BoardCard.jsx
+++ frontend/src/components/BoardList/BoardCard/BoardCard.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import "./BoardCard.css";
 
-export default function BoardCard({board}){
-
-  console.log(board);
-  
-  return(
-
-    <article className = "board-card">
-      <img className = "board-image" src="/placeholderboard.png" alt={`placeholder image`}/>
+export default function BoardCard({ board }) {
+  return (
+    <article className="board-card">
+      <img
+        className="board-image"
+        src="/placeholderboard.png"
+        alt={`placeholder image`}
+      />
       <h2>{board.title}</h2>
       <p>{board.category}</p>
 
-        <div className = "button-container">
-          <button>View Board</button>
-          <button>Delete Board</button>
-        </div>
+      <div className="button-container">
+        <button>View Board</button>
+        <button>Delete Board</button>
+      </div>
     </article>
-  )
-
-
-
-
+  );
 }
