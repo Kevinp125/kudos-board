83035014761b5f7f217e1868fba9426679369e97
Kevin Pereda <kevinpereda125@meta.com>
2025-06-16 18:03:04 -0700

    Got the BoardList grid set up and looking good. As well as the BoardCard component which are the cards that go in the grid. Just need to polish css

---
+++ frontend/src/components/BoardList/BoardCard/BoardCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./BoardCard.css";
+
+export default function BoardCard({board}){
+
+  console.log(board);
+  
+  return(
+
+    <article className = "board-card">
+      <img className = "board-image" src="/placeholderboard.png" alt={`placeholder image`}/>
+      <h2>{board.title}</h2>
+      <p>{board.category}</p>
+
+        <div className = "button-container">
+          <button>View Board</button>
+          <button>Delete Board</button>
+        </div>
+    </article>
+  )
+
+
+
+
+}
--- frontend/src/components/BoardList/BoardList.jsx
+++ frontend/src/components/BoardList/BoardList.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import './boardlist.css'
+import BoardCard from "./BoardCard/BoardCard";
+
+import { boards } from "../../data";
 
 
 export default function BoardList(){
 
   return(
-
-    <>
     
-    </>
+    <section className = "boardlist-container">
+      {boards.map( (board) => <BoardCard key = {board.id} board = {board}/> )}
+    </section>
+
+    
   )
-
-
-
-
 
 }
