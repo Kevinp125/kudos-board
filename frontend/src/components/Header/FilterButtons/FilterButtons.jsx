f1851065953a9394b1038ebaeacb8835920cbd32
Kevin Pereda <kevinpereda125@meta.com>
2025-06-16 17:25:58 -0700

    Got filter buttons included as part of the header and they are nice and styled

---
+++ frontend/src/components/Header/FilterButtons/FilterButtons.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./filterbuttons.css"
+
+export default function FilterButtons(){
+
+  return(
+    <aside className = "filterbtn-aside">
+      <button>All</button>
+      <button>Recent</button>
+      <button>Celebration</button>
+      <button>Thank You</button>
+      <button>Inspiration</button>
+    </aside>
+
+
+
+  )
+
+
+
+
+}  
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './header.css'
 
 import SearchBar from "./SearchBar/SearchBar";
+import FilterButtons from "./FilterButtons/FilterButtons";
 
 
 export default function Header(){
@@ -12,6 +13,7 @@ export default function Header(){
       <img className = "bonsai-img" src= "/bonsai.png" alt="bonsai logo" />
       <h1>KudoBoard</h1>
       <SearchBar/>
+      <FilterButtons/>
     </header>
 
 
