3cf5b534dd197cb57107d1191801f01380a566ba
Kevin Pereda <kevinpereda125@meta.com>
2025-06-16 22:22:38 -0700

    Added a little bit more styling to the post card. Main thing I also added was state for the upvotes so whenever button is clicked the state is updated by incrementing upvote count by one triggering re-render

--- frontend/src/components/PostList/PostCard/PostCard.jsx
+++ frontend/src/components/PostList/PostCard/PostCard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useState } from "react";
 import "./post-card.css";
 
 export default function PostCard({ post }) {
+  const [upvotes, setUpVotes] = useState(post.upvotes);
+
+  function handleUpVoteClick() {
+    setUpVotes((prevUpVotes) => prevUpVotes + 1);
+  }
+
   return (
     <article className="post-card">
       <h2>{post.title}</h2>
@@ -12,8 +19,11 @@ export default function PostCard({ post }) {
         alt={`placeholder image`}
       />
 
+      <p>{post.author}</p>
       <div className="post-btn-container">
-        <button>Upvote</button>
+        <button className="upvotebtn" onClick={handleUpVoteClick}>
+          Upvote: {upvotes}
+        </button>
         <button className="deletebtn">Delete</button>
       </div>
     </article>
